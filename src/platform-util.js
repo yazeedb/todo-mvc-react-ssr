0fd2f52456298cdab1a9c371efd03a0c813ee222
yazeedb <ybzadough@gmail.com>
2017-07-03 16:13:43 -0400

    remove getPlatform method

--- src/platform-util.js
+++ src/platform-util.js
@@ -1,9 +1,6 @@
-// naive check for browser or node environment
+  // naive check for browser or node environment
 
 module.exports = {
-  getPlatform: () => {
-    return typeof window !== 'undefined' && window.document ? 'browser' : 'node';
-  },
   isBrowser: () => typeof window !== 'undefined' && window.document,
   isNode: () => !this.isBrowser()
 }
