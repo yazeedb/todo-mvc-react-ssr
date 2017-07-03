0015c4045dc458d12ee969f5be8b1b6b7f5a79b8
yazeedb <ybzadough@gmail.com>
2017-07-03 16:17:36 -0400

    remove newline and tabbing

--- src/platform-util.js
+++ src/platform-util.js
@@ -1,5 +1,4 @@
-  // naive check for browser or node environment
-
+// naive check for browser or node environment
 module.exports = {
   isBrowser: () => typeof window !== 'undefined' && !!window.document,
   isNode: () => typeof global !== 'undefined' && !!module.exports
