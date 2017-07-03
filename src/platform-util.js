b77aa42bdd281995f883ce3afdcf9f2e773e5a40
yazeedb <ybzadough@gmail.com>
2017-07-03 16:16:38 -0400

    update isNode util method

--- src/platform-util.js
+++ src/platform-util.js
@@ -1,6 +1,6 @@
   // naive check for browser or node environment
 
 module.exports = {
-  isBrowser: () => typeof window !== 'undefined' && window.document,
-  isNode: () => !this.isBrowser()
+  isBrowser: () => typeof window !== 'undefined' && !!window.document,
+  isNode: () => typeof global !== 'undefined' && !!module.exports
 }
