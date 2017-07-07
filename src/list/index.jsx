132a94c9bbc92a61812a970ee92e0d59539d417a
yazeedb <ybzadough@gmail.com>
2017-07-07 09:30:54 -0400

    remove redundant import

--- src/list/index.jsx
+++ src/list/index.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Component } from 'react';
 
 import { ListItem } from '../list-item';
 import ListService from './list.service';
 
-export class List extends Component {
+export class List extends React.Component {
   render() {
     const listItems = ListService.getTodos()
       .map((t, index) => <ListItem todo={t} key={index} />);
